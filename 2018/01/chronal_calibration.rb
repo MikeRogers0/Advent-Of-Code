5a218ac4a1850efdea217c7ccc1e8a2d04fac75b
Mike Rogers <me@mikerogers.io>
2021-01-10 15:33:25 +0000

    Adding failing specs for part 2

--- 2018/01/chronal_calibration.rb
+++ 2018/01/chronal_calibration.rb
@@ -1,11 +1,15 @@
 class ChronalCalibration
-  attr_reader :input
+  FILE_INPUT = File.read("./2018/01/input.txt").split("\n").collect(&:to_i)
 
   # Starting with a frequency of zero,
   # what is the resulting frequency after all of the changes in frequency have been applied?
   def self.part_one
-    input = File.read("./2018/01/input.txt").split("\n").collect(&:to_i)
-    new(input).combined_frequencies
+    new(FILE_INPUT).combined_frequencies
+  end
+
+  # What is the first frequency your device reaches twice?
+  def self.part_two
+    new(FILE_INPUT).frequency_device_reaches_twice
   end
 
   def initialize(input)
@@ -14,5 +18,8 @@ class ChronalCalibration
 
   def combined_frequencies
     @input.inject(&:+)
+  end
+
+  def frequency_device_reaches_twice
   end
 end
--- 2018/01/chronal_calibration_spec.rb
+++ 2018/01/chronal_calibration_spec.rb
@@ -1,6 +1,19 @@
 require "./2018/01/chronal_calibration"
 
 describe ChronalCalibration do
+  describe "::part_one" do
+    subject { described_class.part_one }
+
+    it { expect(subject).to eq(497) }
+  end
+
+  describe "::part_two" do
+    pending
+    subject { described_class.part_two }
+
+    it { expect(subject).to eq(nil) }
+  end
+
   describe "#combined_frequencies" do
     subject { described_class.new(input).combined_frequencies }
 
@@ -23,9 +36,31 @@ describe ChronalCalibration do
     end
   end
 
-  describe "::part_one" do
-    subject { described_class.part_one }
+  describe "#frequency_device_reaches_twice" do
+    subject { described_class.new(input).frequency_device_reaches_twice }
 
-    it { expect(subject).to eq(497) }
+    context "where input is: +1, -1" do
+      let(:input) { [+1, -1] }
+
+      it { expect(subject).to eq(0) }
+    end
+
+    context "where input is: +3, +3, +4, -2, -4" do
+      let(:input) { [+3, +3, +4, -2, -4] }
+
+      it { expect(subject).to eq(10) }
+    end
+
+    context "where input is: -6, +3, +8, +5, -6" do
+      let(:input) { [-6, +3, +8, +5, -6] }
+
+      it { expect(subject).to eq(5) }
+    end
+
+    context "where input is: +7, +7, -2, -7, -4" do
+      let(:input) { [+7, +7, -2, -7, -4] }
+
+      it { expect(subject).to eq(14) }
+    end
   end
 end
