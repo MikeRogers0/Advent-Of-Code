5620d8dfde1b0cc3ae08238c903bb3e883343594
Mike Rogers <me@mikerogers.io>
2021-02-12 17:16:51 +0000

    Solving Roman to Integer challenge

---
+++ Roman-To-Integer/roman_to_integer.rb
@@ -0,0 +1,45 @@
+class RomanToInteger
+  CONVERSIONS = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+
+  attr_accessor :input
+
+  def self.convert_to_i(input)
+    new(input).to_i
+  end
+
+  def initialize(input)
+    @input = input
+  end
+
+  def to_i
+    input_with_modifier_integers.sum
+  end
+
+  private
+
+  # When our input is: IIV
+  # It returns values like:
+  # [-1, -1, 5]
+  def input_with_modifier_integers
+    input_as_literal_integers.each_with_index do |value, index|
+      next unless input_as_literal_integers[index + 1]
+      next unless input_as_literal_integers[index + 1] > input_as_literal_integers[index]
+
+      input_as_literal_integers[index] = input_as_literal_integers[index] * -1
+    end
+  end
+
+  # Convert values like V & X, to 5 & 10
+  def input_as_literal_integers
+    @input_as_literal_integers ||= input.upcase.chars.collect { |letter| CONVERSIONS[letter.to_sym] }
+  end
+
+end
